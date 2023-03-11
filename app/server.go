9b5dcd8446ad6c2ae6024b9902e945f78d0e53c1
RootHex200 <sabiturrahman60@gmail.com>
2023-03-11 21:50:06 +0600

    Respond to PING

--- app/server.go
+++ app/server.go
@@ -2,25 +2,69 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
+	"regexp"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
+
+func nilcheck(err error){
+	if err != nil {
+		fmt.Printf("Failed to bind to port 6379%s",err.Error())
+		os.Exit(1)
+	}
+}
+
+
+func parsedata(data string)(string){
+	re := regexp.MustCompile(`\$\d+\r\n([^\r\n]+)\r\n`)
+    input := data
+	var results []string
+	
+    matches := re.FindAllStringSubmatch(input, -1)
+
+    for _, match := range matches {
+		results = append(results, match[1])
+    }
+	joined := strings.Join(results, " ")
+	results = results[:0]
+	return  joined
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	
+	nilcheck(err)
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		nilcheck(err)
+		go func (conn net.Conn){
+			buf:=make([]byte, 1024)
+			for{
+				n, err := conn.Read(buf)
+				if err != nil {
+					fmt.Printf("Failed to read from connection%s",err.Error())
+					os.Exit(1)
+				}
+				cmd:=parsedata(string(buf[:n]))
+				fmt.Println(strings.ToUpper(cmd))
+				if(strings.ToUpper(cmd)=="PING"){
+					conn.Write([]byte("+PONG\r\n"))
+				}else{
+					conn.Write([]byte(fmt.Sprintf("+%s\r\n",cmd)))
+				}
+				
+			}
+			
+			
+		}(conn)
 	}
 }
