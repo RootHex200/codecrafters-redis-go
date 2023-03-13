d26bf0c0fe349f5d1450036431dc1d9f4f0f570d
RootHex200 <sabiturrahman60@gmail.com>
2023-03-13 19:39:23 +0600

    stage 4 done

--- app/server.go
+++ app/server.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 	"strings"
+	// "strings"
 
 	"regexp"
 
 	// Uncomment this block to pass the first stage
 	"net"
-
 )
 
 
@@ -20,19 +20,19 @@ func nilcheck(err error){
 }
 
 
-func parsedata(data string)(string){
+func parsedata(data string)([]string){
 	re := regexp.MustCompile(`\$\d+\r\n([^\r\n]+)\r\n`)
     input := data
-	var results []string
+	results:=make([]string,0)
 	
     matches := re.FindAllStringSubmatch(input, -1)
-
     for _, match := range matches {
 		results = append(results, match[1])
     }
-	joined := strings.Join(results, " ")
-	results = results[:0]
-	return joined
+	fmt.Printf("results:%s",results)
+	// joined := strings.Join(results, " ")
+	// results = results[:0]
+	return results
 }
 
 func main() {
@@ -55,11 +55,35 @@ func main() {
 					return
 				}
 				cmd:=parsedata(string(buf[:n]))
-				
-				if(strings.ToUpper(cmd)=="PING"){
-					conn.Write([]byte("+PONG\r\n"))
-				}else{
-					conn.Write([]byte(fmt.Sprintf("+%s\r\n",cmd)))
+				fmt.Println("this is cmd:",len(cmd))
+				fmt.Println("this is cmd[0]:",strings.ToUpper(cmd[0]))
+
+				switch strings.ToUpper(cmd[0]) {
+					case "PING":
+						if(len(cmd)==1){
+							conn.Write([]byte("+PONG\r\n"))
+							
+						}else{
+							conn.Write([]byte("+wrong command\r\n"))
+						}
+						
+						
+					case "ECHO":
+						if(len(cmd)==2){
+							conn.Write([]byte("+"+cmd[1]+"\r\n"))							
+						}else{
+							conn.Write([]byte("+wrong command\r\n"))
+						}
+						
+						
+					case "SET":
+						conn.Write([]byte("+OK\r\n"))
+						
+					case "GET":
+						conn.Write([]byte("$"+cmd[1]+"\r\n"))
+						
+					default:
+					conn.Write([]byte("-ERR unknown command '"+cmd[0]+"'\r\n"))
 				}
 				
 			}
